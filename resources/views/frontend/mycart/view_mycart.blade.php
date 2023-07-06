1a785597ecc607a9e7e1bf79073bb4b0e72b9a04
Md Asraful Islam <asraful9355@gmail.com>
2023-07-06 12:18:20 +0600

    coding sundor coupon frontend

--- resources/views/frontend/mycart/view_mycart.blade.php
+++ resources/views/frontend/mycart/view_mycart.blade.php
@@ -41,30 +41,27 @@
          <div class="row mt-50">
             <div class="col-lg-5">
                @if(Session::has('coupon'))
-            
                @else
                <div class="p-40" id="couponField">
-                   <h4 class="mb-10">Apply Coupon</h4>
-                   <p class="mb-30"><span class="font-lg text-muted">Using A Promo Code?</p>
-                   <form action="#">
-                       <div class="d-flex justify-content-between">
-       <input class="font-medium mr-15 coupon" id="coupon_name" placeholder="Enter Your Coupon">
-       
-         <a type="submit" onclick="applyCoupon()" class="btn btn-success"><i class="fi-rs-label mr-10"></i>Apply</a>
-                       </div>
-                   </form>
+                  <h4 class="mb-10">Apply Coupon</h4>
+                  <p class="mb-30"><span class="font-lg text-muted">Using A Promo Code?</p>
+                  <form action="#">
+                     <div class="d-flex justify-content-between">
+                        <input class="font-medium mr-15 coupon" id="coupon_name" placeholder="Enter Your Coupon">
+                        <a type="submit" onclick="applyCoupon()" class="btn btn-success"><i class="fi-rs-label mr-10"></i>Apply</a>
+                     </div>
+                  </form>
                </div>
                @endif 
-           </div>
+            </div>
             <div class="col-lg-7">
                <div class="divider-2 mb-30"></div>
                <div class="border p-md-4 cart-totals ml-30">
                   <div class="table-responsive">
-                      <table class="table no-border">
+                     <table class="table no-border">
                         <tbody id="couponCalField">
-                             
-                          </tbody>
-                      </table>
+                        </tbody>
+                     </table>
                   </div>
                   <a href="#" class="btn mb-20 w-100">Proceed To CheckOut<i class="fi-rs-sign-out ml-15"></i></a>
                </div>
