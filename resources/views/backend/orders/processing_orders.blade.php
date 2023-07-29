d33a6a244c525e9567c648c7a08c5601c17e7e1f
Md Asraful Islam <asraful9355@gmail.com>
2023-07-29 14:46:25 +0600

    return order options oder

--- resources/views/backend/orders/processing_orders.blade.php
+++ resources/views/backend/orders/processing_orders.blade.php
@@ -4,13 +4,13 @@
 <div class="page-content">
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-       <div class="breadcrumb-title pe-3">All Pending Order</div>
+       <div class="breadcrumb-title pe-3">All Processing Order</div>
        <div class="ps-3">
           <nav aria-label="breadcrumb">
              <ol class="breadcrumb mb-0 p-0">
                 <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                 </li>
-                <li class="breadcrumb-item active" aria-current="page">All Pending Order</li>
+                <li class="breadcrumb-item active" aria-current="page">All Processing Order</li>
              </ol>
           </nav>
        </div>
@@ -46,8 +46,7 @@
                       <td>{{ $item->payment_method }}</td>
                       <td> <span class="badge rounded-pill bg-success"> {{ $item->status }}</span></td>
                       <td>
-                        <a href="{{ route('admin.order.details',$item->id) }}" class="btn btn-info" title="Details"><i class="fa fa-eye"></i> </a>
-
+                         <a href="{{ route('admin.order.details',$item->id) }}" class="btn btn-info" title="Details"><i class="fa fa-eye"></i> </a>
                       </td>
                    </tr>
                    @endforeach
